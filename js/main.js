1265969f7c6ed6d475c9cddb4b898b866c97c0bf
Ramo01 <r.murtuzayev@list.ru>
2022-04-26 12:33:57 +0400

    gigaland "uptade"

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
 let icon=document.querySelector(".bars");
 let navSide=document.querySelector(".navSide")
-
 icon.addEventListener("click", function(){
      navSide.classList.toggle("mobile")
+})
+let header=document.querySelector(".header");
+window.onscroll=function(){
+    if(window.scrollY>50){
+        header.classList.add("header-active");
+
+    }
+    else{
+        header.classList.remove("header-active");
+    }
+}
+
+let toTop=document.querySelector(".to-top");
+
+window.addEventListener("scroll", ()=>{
+    if(window.pageYOffset > 100){
+        toTop.classList.add("active")
+    }
+    else{
+        toTop.classList.remove("active")
+    }
 })
